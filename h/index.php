4c62484062d18d920f3dcfbb7c23b0d7673baedc
Sunisa03 <66010914020@msu.ac.th>
2026-02-04 15:25:40 +0700

    a

--- h/index.php
+++ h/index.php
@@ -17,5 +17,28 @@ Password <input type="password" name="apwd" required> <br>
 <button type="submit" name="Submit">LOGIN</button>
 </form>
 
+<?php
+if(isset($_POST['Submit'])){
+    include_once("connectdb.php");
+    $sql = "SELECT * FORM admin WHERE a_username='{$_POST['auser']}'AND a_password='{$_POST['apwd']}'LIMIT 1";
+    $rs = mysqli_query($conn,$sql);
+    $num = mysqli_num_rows($rs);
+
+    if($num == 1){
+        $data = mysqli_num_rows($rs);
+        $_SESSION['aid'] = $data['a_id'];
+        $_SESSION['aname'] = $data['a_name'];
+        echo "<script>";
+        echo "window.location='index2.php';";
+        echo "</script>";
+    }else{
+        echo "<script>";
+        echo "alert('Username หรือ Password ไม่ถูกต้อง');";
+        echo "</script>";
+    }
+
+}
+?>
+
 </body>
 </html>
