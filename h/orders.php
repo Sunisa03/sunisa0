9e1cecb1753aba665c99b5a579277dd754d0f48e
Sunisa03 <66010914020@msu.ac.th>
2026-02-04 16:55:47 +0700

    a

--- h/orders.php
+++ h/orders.php
@@ -1,29 +1,21 @@
-<?php
-    session_start();
-    if (empty($_SESSION['aid'])){
-        echo "Access Denied";
-        echo "<meta http-equiv='refresh' content='3; url=index.php'";
-        exit;
-    }
-?>
 <!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
-<title>Sunisa</title>
+<title>จัดการออเดอร์ - สุนิสา</title>
 </head>
 
 <body>
-<h1>หน้าหลักแอดมิน - สุนิสา จันทัน (เนย)</h1>
 
-<?php each "แอดมิน:". $_SESSION['aname']; ?> <br>
+<h1>จัดการออเดอร์ -สุนิสา</h1>
+
+<?php echo "แอดมิน: ". $_SESSION['a_name']; ?> <br>
 
 <ul>
-    <a hreh="products.php"><li>จัดการสินค้า</li></a>
-    <a hreh="orders.php"><li>จัดการออเดอร์</li></a>
-    <a hreh="customers.php"><li>จัดการลูกค้า</li></a>
-    <a hreh="logout.php"><li>ออกจากระบบ</li></a>
-
+	<a href="products.php"><li>จัดการสินค้า</li></a>
+	<a href="orders.php"><li>จัดการออเดอร์</li></a>
+	<a href="customers.php"><li>จัดการลูกค้า</li></a>
+	<a href="logout.php"><li>ออกจากระบบ</li></a>
 </ul>
 </body>
 </html>
