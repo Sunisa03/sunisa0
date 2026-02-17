1e2a5ab31380ddb9ce6c6bcc777fe0e0ae760390
sunisa0 <66010914020@msu.ac.th>
2026-02-17 19:03:58 +0700

    a

--- i/a.php
+++ i/a.php
@@ -9,6 +9,20 @@
 
 <h1>งาน i -- สุนิสา จันทัน (เนย)</h1>
 
+<form method="post" action="">
+    ชื่อภาค <input type="text" name="rname" autofocus required>
+    <button type="submit" name="Submit">บันทึก</button>
+</form><br><br>
+
+<?php
+if(isset($_POST['Submit'])){
+    include_once("connectdb.php");
+    $rname = $_POST['rname'];
+    $sql2 = "INSERT INTO `regions` (`i_id`, `r_name`) VALUES (NULL,'{$rname}')";
+	mysqli_query($conn, $sql2) or die ("เพิ่มข้อมูลไม่ได้");"
+}
+?>
+
 <table border="1">
     <tr>
         <th>รหัสภาค</th>
