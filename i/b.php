af21c102c4c388b16f46efb9be7f5766c740aec4
sunisa0 <66010914020@msu.ac.th>
2026-02-17 21:13:37 +0700

    a

--- i/b.php
+++ i/b.php
@@ -9,50 +9,53 @@
 
 <h1>งาน i -- สุนิสา จันทัน (เนย)</h1>
 
-<form method="post" action="" enctype="multipart/form-data">
-    ชื่อจังหวัด <input type="text" name="pname" autofocus required><br>
-    รูปภาพ <input type="file" name="pimage" required><br>
-
+<form method="post" action="" enctype="multipart/form-data" >
+	ชื่อจังหวัด <input type="text" name="pname" autofocus required><br>
+    รูปภาพ <input type="file" name="pimage" required> <br>
+    
     ภาค
     <select name="rid">
- <?php
+<?php
 include_once("connectdb.php");
-$sql = "SELECT * FROM regions";
-$rs = mysqli_query($conn, $sql);
- while ($data = mysqli_fetch_array($rs)){
-?> 
-        <option value="<?php echo $data['r_id'] ; ?>"><?php echo $data['r_name'] ;?></option>
+$sql3 = "SELECT * FROM regions";
+$rs3 = mysqli_query($conn, $sql3);
+ while ($data3 = mysqli_fetch_array($rs3)){
+?>   
+    	<option value="<?php echo $data3['r_id'] ; ?>"><?php echo $data3['r_name'] ;?></option>
 <?php } ?>
     </select>
-
+    <br>
+    
     <button type="submit" name="Submit">บันทึก</button>	
 </form><br><br>
 
 <?php
 if(isset($_POST['Submit'])){
 	include_once("connectdb.php");
-	$rname = $_POST['pname'];
-    $ext = pathinfo($_FILES['pimage']['name'], PATHINFO_EXTENSION);
-    $rid = $_POST['rid'];
-
-	$sql2 = "INSERT INTO provinces (p_id, p_name, p_ext, r_id) VALUES (NULL, '{$pname}','{$ext}','{$rid}')";
+	$pname = $_POST['pname'];
+	$ext = pathinfo($_FILES['pimage']['name'], PATHINFO_EXTENSION);
+	$rid = $_POST['rid'];
+	
+	$sql2 = "INSERT INTO provinces  VALUES (NULL, '{$pname}', '{$ext}', '{$rid}')";
 	mysqli_query($conn, $sql2) or die ("เพิ่มข้อมูลไม่ได้");
-    $pid = mysqli_insert_id($conn);
-    copy($_FILES['pimage']['tmp_name'],"images/".$pid.".".$ext);
-
+	$pid = mysqli_insert_id($conn);
+	//copy($_FILES['pimage']['tmp_name'], "images/".$pid.".".$ext);
+	move_uploaded_file($_FILES['pimage']['tmp_name'], "images/".$pid.".".$ext);
 }
 ?>
 
+
 <table border="1">
-    <tr>
-        <th>รหัสจังหวัด</th>
+	<tr>
+    	<th>รหัสจังหวัด</th>
         <th>ชื่อจังหวัด</th>
+        <th>ชื่อภาค</th>
         <th>รูป</th>
         <th>ลบ</th>
     </tr>
-    <?php
+<?php
 include_once("connectdb.php");
-$sql = "SELECT * FROM provinces";
+$sql = "SELECT * FROM provincesAS p INNER JOIN regions AS r ON p.r_id=r.r_id";
 $rs = mysqli_query($conn, $sql);
  while ($data = mysqli_fetch_array($rs)){
 ?>   
@@ -60,11 +63,15 @@ $rs = mysqli_query($conn, $sql);
     	<td><?php echo $data['p_id'] ; ?></td>
         <td><?php echo $data['p_name'] ;?></td>
         <td><?php echo $data['r_name'] ;?></td>
-        <td><img src="images/<?php echo $data['p_id'];?>.<?php echo $data['p-ext'];?>"width="100"></td>
-        <td width="80" align="center"><a href="delete_provinces.php?id=<?php echo $data['r_id']; ?>&ext=<?php echo $data['p_ext'];?>" onClick="return confirm('ยืนยันการลบ?');"><img src="images/delete.jpg" width="50"></a></td>
+        <td><img src="images/<?php echo $data['p_id'] ; ?>.<?php echo $data['p_ext'] ; ?>" width="140"></td>
+        <td width="80" align="center"><a href="delete_provinces.php?id=<?php echo $data['p_id']; ?>&ext=<?php echo $data['p_ext'] ;?>" onClick="return confirm('ยืนยันการลบ');"><img src="images/delete.jpg" width="20"></a></td>
     </tr>
 <?php } ?>
 </table>
 
 </body>
 </html>
+
+<?php
+mysqli_close($conn);
+?>
