270a455604bc010502afc112183f9a847721334d
Sunisa03 <66010914020@msu.ac.th>
2026-02-18 15:32:01 +0700

    a

--- i/connectdb.php
+++ i/connectdb.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pwd = "66010914005"; 
-$db = "4024db";
+$db = "4020db";
 
 $conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลล้มเหลว");
 mysqli_query($conn, "SET NAMES utf8");
