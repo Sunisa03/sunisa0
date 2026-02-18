be555b775b895d833913aa3f1b2b8be8b476ba43
Sunisa03 <66010914020@msu.ac.th>
2026-02-18 16:56:08 +0700

    a

---
+++ k/a.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>sunisa</title>
+</head>
+
+<body>
+
+<h1>66010914020 สุนิสา จันทัน (เนย)</h1>
+
+<button type="button" 
+        style="background-color: green; color: white;"
+        onclick=<img src="img/2.jpeg" width="30">>
+</button>
+
+<button type="button" 
+        style="background-color: yellow;"
+        onclick="window.open('รูป2.jpg')">
+ 
+</button>
+
+</body>
+</html>
