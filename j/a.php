14fed1e1c836e0b1300692ea2be92afa84cd33e0
Sunisa03 <66010914020@msu.ac.th>
2026-02-18 13:34:30 +0700

    a

---
+++ j/a.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<h1>งาน j -- สุนิสา จันทัน (เนย)</h1>
+<img src="1.jpeg" height="100">
+</body>
+</html>
